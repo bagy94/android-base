b1daddedd1f6d0c59d356bd32c358a064c6f8360
bagy94 <davor.bagaric@factory.hr>
2020-09-25 10:09:58 +0200

    Update BottomSheetFragment.kt

--- base/src/main/java/hr/bagy94/android/base/bottomsheet/BottomSheetFragment.kt
+++ base/src/main/java/hr/bagy94/android/base/bottomsheet/BottomSheetFragment.kt
@@ -1,10 +1,23 @@
 package hr.bagy94.android.base.bottomsheet
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 abstract class BottomSheetFragment <BINDING:ViewDataBinding>: BottomSheetDialogFragment(){
-    abstract val binding : BINDING
+    lateinit var binding : BINDING private set
     abstract val layoutId : Int
 
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = onCreateViewBinding(inflater,container)
+        return binding.root
+    }
+
+    protected open fun onCreateViewBinding(inflater: LayoutInflater, container: ViewGroup?): BINDING {
+        return DataBindingUtil.inflate(inflater, layoutId, container, false)
+    }
 }
